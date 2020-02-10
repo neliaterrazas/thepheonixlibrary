9067dcdadb65671f9440f45bcb63d6de2426b0db
Nelia Terrazas <terrazasnelia@gmail.com>
2020-02-10 12:27:39 -0800

    photogrid work

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Links from './Links';
+import mujer2 from '../assets/imgs/Adjustments.jpeg';
+
+
+function Header() {
+    return (
+        <div>
+            <style jsx>{
+
+            }</style>
+            <img src={mujer2}/>
+            <Links/>
+        </div>
+    );
+}
+
+export default Header;
