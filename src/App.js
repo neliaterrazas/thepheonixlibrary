b0a3d9737f74321729c912c9b1255ff3dc457771
Nelia Terrazas <terrazasnelia@gmail.com>
2020-02-13 20:26:34 -0800

    new branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
-import mujer from './assets/imgs/mujer.jpeg';
+// import mujer from './assets/imgs/mujer.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-        <img src={mujer}  alt="x" />
+        {/* <img src={mujer}  alt="x" /> */}
     </div>
   );
 }
