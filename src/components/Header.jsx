9aa118a4bdc69e6f0a2c9f2771d9b5bd23c28821
Nelia Terrazas <terrazasnelia@gmail.com>
2020-02-20 21:39:07 -0800

    add material ui and nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,57 @@
 import React from 'react';
 import Links from './Links';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu'; 
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
+export default function ButtonAppBar() {
+  const classes = useStyles();
 
-function Header() {
-    return (
-        <div className="header">
-            <style jsx>{`
-            .header {
-                border: 2px solid red;
-            }`
-
-            }</style>
-            <Links/>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            The Pheonix Library
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Header;
+
+
+// function Header() {
+//     return (
+//         <div className="header">
+//             <style jsx>{`
+//             .header {
+//                 border: 2px solid red;
+//             }`
+//             }</style>
+//             <Links/>
+//         </div>
+//     );
+// }
+
+// export default Header;
